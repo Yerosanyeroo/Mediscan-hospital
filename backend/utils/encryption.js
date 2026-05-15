284946d7aa132ffd5e4148fc81ae020d83fda914
Yerosanyeroo <yerosanyeroo77@gmail.com>
2026-05-15 17:21:51 +0300

    Delete backend directory

--- backend/utils/encryption.js
+++
@@ -1,9 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-class EncryptionService {
-  hashFaceEmbedding(embedding) {
-    return CryptoJS.SHA256(JSON.stringify(embedding)).toString();
-  }
-}
-
-module.exports = new EncryptionService();
